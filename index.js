fe129cfaa35be262fb0b2fb9b20b17008fe6619c
Ashmit Ranjan <ashmitranjan978@gmail.com>
2023-05-15 18:11:51 +0530

    added cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use((req,res,next)=>{
-    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin","https://easyfood-yotx.onrender.com");
     res.header(
         "Access-Control-Allow-Headers",
         "Origin,X-Requested-With,Content-Type,Accept"
